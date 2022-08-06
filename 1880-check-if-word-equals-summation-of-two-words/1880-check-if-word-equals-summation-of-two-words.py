f75d52cbad17bc70de164d214f0a83e5b344acd1
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-08-06 14:58:51 +0530

    Time: 46 ms (59.15%), Space: 13.9 MB (27.76%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        k = list(map(chr, range(97, 123)))
+        v = range(0,26)
+        res = {k[i]: v[i] for i in range(len(k))}
+        # # # # # print(res)
+        m = list(firstWord)
+        c=""
+        for n in m:
+            c += str(res[n])
+        # # # # print(c)
+        m1 = list(secondWord)
+        c1=""
+        for n1 in m1:
+            c1 += str(res[n1])
+        # # # print(c1)
+        k1 = int(c1)+int(c)
+        # # print(k1)
+        m2 = list(targetWord)
+        c2=""
+        for i2 in m2:
+            c2 += str(res[i2])
+        # print(c2)
+        if int(c2) == k1:
+            return True
+        else:
+            return False

f447ae5af168a8be26f45bbf675e216e953aa8e1
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-09-02 18:15:47 +0530

    Time: 125 ms (43.00%), Space: 13.8 MB (99.10%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -1,20 +1,3 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        num_1 = set(nums1)
-        num_2 = set(nums2)
-        num_3 = set(nums3)
-        k = []
-        n12 = num_1.intersection(num_2)
-        for i in n12:
-            k.append(i)
-        n13 = num_1.intersection(num_3)
-        for j in n13:
-            k.append(j)
-        n32 = num_3.intersection(num_2)
-        for l in n32:
-            k.append(l)
-        n123 = n32.intersection(num_1)
-        for p in n123:
-            k.append(p)
-        return (list(set(k)))
-        
+        return set(nums1) & set(nums2) | set(nums2) & set(nums3) | set(nums1) & set(nums3)
